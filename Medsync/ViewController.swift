a9866b47976a3b1d4fff0745e783064e3a74bb99
Jaimin Bhagat <63891526+Jaimin-Bhagat@users.noreply.github.com>
2025-03-16 15:57:05 -0400

    Initial Commit

---
+++ Medsync/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Medsync
+//
+//  Created by JAIMIN BHAGAT on 2025-03-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MedsyncTests/MedsyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  MedsyncTests.swift
+//  MedsyncTests
+//
+//  Created by JAIMIN BHAGAT on 2025-03-16.
+//
+
+import Testing
+@testable import Medsync
+
+struct MedsyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
